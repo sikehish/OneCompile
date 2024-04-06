414aa3966ba68316a39137e9540e421e786dce38
sikehish <hisham0502@gmail.com>
2024-04-06 18:12:43 +0530

    Added CORS support

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,13 +1,24 @@
 package server
 
 import (
+	"github.com/gin-contrib/cors" // Import the cors package
 	"github.com/gin-gonic/gin"
 	"github.com/sikehish/OneCompile/internal/handlers"
 )
 
 func newRouter() *gin.Engine {
 	r := gin.Default()
-	//Testing if the API's working
+
+	// Apply CORS middleware
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"http://localhost:5173",
+		},
+		AllowMethods: []string{"GET", "POST"},
+		AllowHeaders: []string{"Origin", "Content-Type"},
+	}))
+
+	// Testing if the API's working
 	r.GET("/test", handlers.TestHandler)
 	r.POST("/execute", handlers.Execute)
 	return r
@@ -15,5 +26,6 @@ func newRouter() *gin.Engine {
 
 func RunServer(addr string) error {
 	router := newRouter()
+
 	return router.Run(addr)
 }
